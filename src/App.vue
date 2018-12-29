3c9ef694f2865d4463aec09d88c6f4701ebe3238
NgeKaworu <NgeKaworu@163.com>
2018-12-29 11:00:15 +0800

    官方例子todolist试水

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,20 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TodosList />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import TodosList from './components/todosList/TodosList.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    TodosList
+
   }
 }
 </script>
