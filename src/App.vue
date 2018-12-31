97ebfffd183d8675e91e67d19ae9785772f7559e
NgeKaworu <NgeKaworu@163.com>
2018-12-31 08:08:45 +0800

    路由层

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,71 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld my-message="Welcome to Your Vue.js App"  />
-    <TodosList @event-test="onEventTest" />
-    <Sudoku />
-  </div>
+  <Layout>
+    <Header class='header'>
+      <Menu mode="horizontal">
+        <Item key="home">
+          <RouterLink to="/">
+            <Icon type="home" />Home</RouterLink>
+        </Item>
+        <Item key="shoplist">
+          <RouterLink to="/shoplist">
+            <Icon type="bars" />Shop List
+          </RouterLink>
+        </Item>
+        <Item key="detail">
+          <RouterLink to="/detail">
+            <Icon type="appstore" />Detail
+          </RouterLink>
+        </Item>
+        <Item key="other">
+          <RouterLink to="/other">
+            <Icon type="ellipsis" />Other
+          </RouterLink>
+        </Item>
+      </Menu>
+    </Header>
+    <Content>
+      <RouterView/>
+    </Content>
+    <Footer>Footer</Footer>
+  </Layout>
 </template>
 
 <script>
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld: () => import('./components/HelloWorld'),
-    TodosList: () => import('./components/TodosList'),
-    Sudoku: () => import('./components/Sudoku')
-  },
-  methods:{
-    onEventTest: event => {
-      console.log('子组件回调', event)
-    }
+  import {
+    Layout,
+    Menu,
+    Icon
+  } from 'ant-design-vue';
+  
+  
+  const {
+    Header,
+    Footer,
+    Content,
+  } = Layout;
+  
+  const {
+    Item
+  } = Menu;
+  
+  export default {
+    name: 'app',
+    components: {
+      Layout,
+      Header,
+      Footer,
+      Content,
+      Menu,
+      Item,
+      Icon,
+    },
   }
-}
 </script>
 
 <style scoped>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .header {
+    text-align: center;
+    background: #fff;
+  }
+
 </style>
---
+++ src/router.js
@@ -0,0 +1,48 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'hash',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('./views/Home.vue')
+    },
+    {
+      path: '/shoplist',
+      name: 'shoplist',
+      component: () => import('./views/ShopList.vue')
+    },
+    {
+      path: '/detail',
+      name: 'detail',
+      component: () => import('./views/Detail.vue')
+    },
+    {
+      path: '/other',
+      name: 'other',
+      component: () => import('./views/Other.vue'),
+      children:[
+        {
+          path: '/helloworld',
+          name: 'helloworld',
+          component: () => import('./components/HelloWorld.vue')
+        },
+        {
+          path: '/sudoku',
+          name: 'sudoku',
+          component: () => import('./components/Sudoku.vue')
+        },
+        {
+          path: '/todoslist',
+          name: 'todoslist',
+          component: () => import('./components/TodosList.vue')
+        },
+      ] 
+    }
+  ]
+})
