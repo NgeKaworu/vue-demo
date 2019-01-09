15eb54ed83042a8dac1577db3a352f614baf3079
NgeKaworu <NgeKaworu@163.com>
2019-01-09 23:12:16 +0800

    力图完成

--- src/components/D3Demo/D3Force.vue
+++ src/components/D3Demo/D3Force.vue
@@ -1,5 +1,5 @@
 <template>
-  <div ref="node">123</div>
+  <div ref="node"></div>
 </template>
 
 <script>
@@ -7,22 +7,6 @@ import * as d3 from "d3";
 export default {
   methods: {
     renderChart() {
-      const dragstarted = d => {
-        if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-        d.fx = d.x;
-        d.fy = d.y;
-      };
-
-      const dragged = d => {
-        d.fx = d3.event.x;
-        d.fy = d3.event.y;
-      };
-
-      const dragended = d => {
-        if (!d3.event.active) simulation.alphaTarget(0);
-        d.fx = null;
-        d.fy = null;
-      };
       // 节点
       const node = this.$refs.node;
       // 清除旧数据
@@ -32,24 +16,24 @@ export default {
       //画布大小
       const width = 800;
       const height = 600;
-
+      // 生成假
       const range = 100;
       const data = {
-        nodes: d3.range(0, range).map(d => {
-          return { label: "l" + d, r: ~~d3.randomUniform(8, 28)() };
-        }),
-        links: d3.range(0, range).map(function() {
-          return {
-            source: ~~d3.randomUniform(range)(),
-            target: ~~d3.randomUniform(range)()
-          };
-        })
+        nodes: d3
+          .range(0, range)
+          .map(d => ({ label: "l" + d, r: ~~d3.randomUniform(8, 28)() })),
+        links: d3.range(0, range).map(() => ({
+          source: ~~d3.randomUniform(range)(),
+          target: ~~d3.randomUniform(range)()
+        }))
       };
 
       const margin = { top: 0, left: 0, bottom: 0, right: 0 };
 
       const chartWidth = width - (margin.left + margin.right);
       const chartHeight = height - (margin.top + margin.bottom);
+
+      const color = d3.scaleOrdinal(d3.schemeCategory10);
 
       const svg = d3
         .select(node)
@@ -67,25 +51,21 @@ export default {
 
       const simulation = d3
         .forceSimulation()
+        // 力之间的连线 相互作用
+        .force("link", d3.forceLink().id(d => d.index))
         .force(
-          "link",
-          d3.forceLink().id(d => {
-            return d.index;
-          })
-        )
-        .force(
+          // 斥力
           "collide",
-          d3
-            .forceCollide(d => {
-              return d.r + 8;
-            })
-            .iterations(16)
+          d3.forceCollide(d => d.r + 8).iterations(16)
         )
+        // 电荷力
         .force("charge", d3.forceManyBody())
+        // 中心力
         .force("center", d3.forceCenter(chartWidth / 2, chartHeight / 2))
         .force("y", d3.forceY(0))
         .force("x", d3.forceX(0));
 
+      // 连线
       const link = svg
         .append("g")
         .attr("class", "links")
@@ -95,6 +75,7 @@ export default {
         .append("line")
         .attr("stroke", "black");
 
+      // 圆
       const circle = svg
         .append("g")
         .attr("class", "nodes")
@@ -102,39 +83,52 @@ export default {
         .data(data.nodes)
         .enter()
         .append("circle")
-        .attr("r", d => {
-          return d.r;
-        })
+        .attr("r", d => d.r)
+        .attr("fill", d => color(d.r))
         .call(
           d3
             .drag()
-            .on("start", dragstarted)
-            .on("drag", dragged)
-            .on("end", dragended)
+            // 拖拽事件
+            .on("start", d => {
+              if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+              d.fx = d.x;
+              d.fy = d.y;
+            })
+            .on("drag", d => {
+              d.fx = d3.event.x;
+              d.fy = d3.event.y;
+            })
+            .on("end", d => {
+              if (!d3.event.active) simulation.alphaTarget(0);
+              d.fx = null;
+              d.fy = null;
+            })
         );
 
-      const ticked = function() {
-        link
-          .attr("x1", d => {
-            return d.source.x;
-          })
-          .attr("y1", d => {
-            return d.source.y;
-          })
-          .attr("x2", d => {
-            return d.target.x;
-          })
-          .attr("y2", d => {
-            return d.target.y;
-          });
+      // 文本
+      const texts = svg
+        .append("g")
+        .attr("class", "texts")
+        .selectAll("text")
+        .data(data.nodes)
+        .enter()
+        .append("text")
+        .style("fill", "black")
+        .attr("dx", 20)
+        .attr("dy", 8)
+        .text(d => d.label);
 
-        circle
-          .attr("cx", d => {
-            return d.x;
-          })
-          .attr("cy", d => {
-            return d.y;
-          });
+      // 更新力的相互作用
+      const ticked = () => {
+        link
+          .attr("x1", d => d.source.x)
+          .attr("y1", d => d.source.y)
+          .attr("x2", d => d.target.x)
+          .attr("y2", d => d.target.y);
+
+        circle.attr("cx", d => d.x).attr("cy", d => d.y);
+
+        texts.attr("x", d => d.x).attr("y", d => d.y);
       };
 
       simulation.nodes(data.nodes).on("tick", ticked);
